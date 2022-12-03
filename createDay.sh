f8e7bce39c9be4f7c9f88eb3facd5efaa4c61856
Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
2022-12-02 21:39:00 -0800

    Fix main README.md, createDay.sh

--- createDay.sh
+++ createDay.sh
@@ -12,7 +12,7 @@ fi
 mkdir -p ./src/main/kotlin/
 mkdir -p ./src/inputs
 
-touch ./src/inputs/Day_${DAY}.txt
+touch ./src/inputs/Day_"${DAY}".txt
 
 cp -R ./template ./src/main/kotlin/day_"$DAY"
 
@@ -20,6 +20,6 @@ sed -i "s/package template/package day_$DAY/; s/input.txt/Day_$DAY.txt/" "./src/
 
 printf "# Advent Of Code 2022 Day %s: %s\n\n## Part 1\nTBD\n\n---\n## Part 2\nTBD" "$DAY" "$TITLE" > ./src/main/kotlin/day_"$DAY"/README.md
 
-printf "\n[**Day %s**](/src/main/kotlin/day_%s/)\n - [AdventOfCode](https://adventofcode.com/2022/day/%s)" "$PROVIDED_DAY" "$DAY" "$PROVIDED_DAY" >> README.md
+printf "\n[**Day %s: %s**](/src/main/kotlin/day_%s/) - [AdventOfCode](https://adventofcode.com/2022/day/%s)\n" "$PROVIDED_DAY" "$TITLE" "$DAY" "$PROVIDED_DAY" >> README.md
 
 curl --cookie "session=$AOC_COOKIE" https://adventofcode.com/2022/day/"$PROVIDED_DAY"/input > ./src/inputs/Day_"$DAY".txt
