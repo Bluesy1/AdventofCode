132f8c4eac4cf990050728eb6feb8ce369e392a0
Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
2022-12-04 08:11:59 -0800

    Update Day 4 code to use sets

--- src/main/kotlin/day_04/Main.kt
+++ src/main/kotlin/day_04/Main.kt
@@ -4,34 +4,22 @@ import java.io.File
 
 fun part1(input: List<String>) {
     val res = input.stream().mapToInt {
-        val range1 = it.split(',')[0]
-        val range2 = it.split(',')[1]
-        val start1 = range1.split('-')[0].toInt()
-        val end1 = range1.split('-')[1].toInt()
-        val start2 = range2.split('-')[0].toInt()
-        val end2 = range2.split('-')[1].toInt()
-        if (start1 <= start2 && end1 >= end2) {
-            1
-        } else if (start2 <= start1 && end2 >= end1) {
-            1
-        } else {
-            0
-        }
+        val range1str = it.split(',')[0]
+        val range2str = it.split(',')[1]
+        val range1 = (range1str.split('-')[0].toInt()..range1str.split('-')[1].toInt()).toSet()
+        val range2 = (range2str.split('-')[0].toInt()..range2str.split('-')[1].toInt()).toSet()
+        if (range1.containsAll(range2) || range2.containsAll(range1)) 1 else 0
     }.sum()
     print("The number of contained assignment pairs is $res")
 }
 
 fun part2(input: List<String>) {
     val res = input.stream().mapToInt {
-        val range1 = it.split(',')[0]
+        val range1str = it.split(',')[0]
         val range2str = it.split(',')[1]
-        val range2 = range2str.split('-')[0].toInt()..range2str.split('-')[1].toInt()
-        for (i in range1.split('-')[0].toInt()..range1.split('-')[1].toInt()) {
-            if (i in range2) {
-                return@mapToInt 1
-            }
-        }
-        0
+        val range1 = (range1str.split('-')[0].toInt()..range1str.split('-')[1].toInt()).toSet()
+        val range2 = (range2str.split('-')[0].toInt()..range2str.split('-')[1].toInt()).toSet()
+        if (range1.intersect(range2).isNotEmpty()) 1 else 0
     }.sum()
     print("The number of overlapping assignment pairs is $res")
 }
