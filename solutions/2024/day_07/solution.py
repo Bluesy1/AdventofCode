8f9f30c79632fa853b791837f2eeb121b64332fd
Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
2024-12-06 21:51:50 -0800

    Slightly cleaner day 7?
    Doesn't have any noticeable perf gains - RTL would probably perform better but meh

--- solutions/2024/day_07/solution.py
+++ solutions/2024/day_07/solution.py
@@ -1,54 +1,34 @@
 # puzzle prompt: https://adventofcode.com/2024/day/7
 
 import functools
+import itertools
 from operator import add, mul
+from typing import Callable
 
 from ...base import StrSplitSolution, answer
 from ...utils.tools import *
 
 
+def eval_(target: int, operands: list[int], operations: list[Callable[[int, int], int]]) -> int:
+    ret = operands[0]
+    for operand, op in zip(operands[1:], operations):
+        ret = op(ret, operand)
+        if ret > target:
+            return -1
+    return ret
+
 class Solution(StrSplitSolution):
     _year = 2024
     _day = 7
 
-    @answer(20281182715321)
-    def part_1(self) -> int:
-        ans = 0
-        ops = (add, mul)
+    @answer((20281182715321, 159490400628354))
+    def solve(self) -> tuple[int, int]:
+        p1 = p2 = 0
         for line in self.input:
             target, *inps = nums(line)
-            if functools.reduce(add, inps) == target:
-                ans += target
-                continue
-            if functools.reduce(mul, inps) == target:
-                ans += target
-                continue
-
-            # try all possible combinations of operations
-            tmp = {inps[0]}
-            for i in range(1, len(inps)):
-                tmp = {op(t, inps[i]) for op in ops for t in tmp}
-            if target in tmp:
-                ans += target
-        return ans
-
-    @answer(159490400628354)
-    def part_2(self) -> int:
-        ans = 0
-        ops = (add, mul, lambda a, b: int(f"{a}{b}"))
-        for line in self.input:
-            target, *inps = nums(line)
-            if functools.reduce(add, inps) == target:
-                ans += target
-                continue
-            if functools.reduce(mul, inps) == target:
-                ans += target
-                continue
-
-            # try all possible combinations of operations
-            tmp = {inps[0]}
-            for i in range(1, len(inps)):
-                tmp = {op(t, inps[i]) for op in ops for t in tmp}
-            if target in tmp:
-                ans += target
-        return ans
+            if target in map(functools.partial(eval_, target, inps), itertools.product((add, mul), repeat=len(inps)-1)):
+                p1 += target
+                p2 += target
+            elif target in map(functools.partial(eval_, target, inps), itertools.product((add, mul, lambda a, b: int(f"{a}{b}")), repeat=len(inps)-1)):
+                p2 += target
+        return p1, p2
