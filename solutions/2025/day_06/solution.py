5297bf217697ca89e09ec1e73aa9c6051d475a9d
Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
2025-12-05 21:26:29 -0800

    2025 Day 6
    Part 1: 00:04:55
    Part 2: 00:22:10
    
    Probably a better way to do this, but a nice use of functional programming, and still super fast!
    
    Struggled a bit on part 2 though with how I needed to parse the numbers as strings not ints to keep spacing so I couldn't use my premade `nums` function

---
+++ solutions/2025/day_06/solution.py
@@ -0,0 +1,33 @@
+# puzzle prompt: https://adventofcode.com/2025/day/6
+
+from ...base import StrSplitSolution, answer
+from ...utils.tools import *
+
+import functools
+from operator import mul, add
+
+class Solution(StrSplitSolution):
+    _year = 2025
+    _day = 6
+
+    @answer((4805473544166, 8907730960817))
+    def solve(self) -> tuple[int, int]:
+        *lines, ops = self.input
+        ops = [mul if op == "*" else add for op in ops.replace(" ", "")]
+        l = len(lines[0])
+        sets: list[tuple[str, ...]] = []
+        start = 0
+        for i in range(l):
+            if all(line[i] == " " for line in lines):
+                sets.append(tuple(line[start:i] for line in lines))
+                start = i + 1
+        sets.append(tuple(line[start:] for line in lines))
+        part1 = sum(
+            functools.reduce(op, map(int, nums))
+            for op, nums in zip(ops, sets)
+        )
+        part2 = sum(
+                    functools.reduce(op, (int("".join(s for s in i if s != ' ')) for i in itertools.zip_longest(*nums)))
+                    for op, nums in zip(ops, sets)
+                )
+        return part1, part2
