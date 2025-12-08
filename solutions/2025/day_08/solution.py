9d087ad587e4f57f67a844862e2eb0d7b17b5aed
Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
2025-12-08 10:40:52 -0800

    Optimize 2025 Day 8

--- solutions/2025/day_08/solution.py
+++ solutions/2025/day_08/solution.py
@@ -1,13 +1,20 @@
 # puzzle prompt: https://adventofcode.com/2025/day/8
 
+import itertools
 import math
-
-import networkx
 
 from ...base import StrSplitSolution, answer
 from ...utils.tools import *
 
 type Node = tuple[int, int, int]
+
+
+def geo3D(node1: Node, node2: Node, /) -> float:
+    return (
+        (node1[0] - node2[0]) ** 2
+        + (node1[1] - node2[1]) ** 2
+        + (node1[2] - node2[2]) ** 2
+    )
 
 
 class Solution(StrSplitSolution):
@@ -17,47 +24,30 @@ class Solution(StrSplitSolution):
     @answer((103488, 8759985540))
     def solve(self) -> tuple[int, int]:
         boxes: list[Node] = [tuple(nums(box)) for box in self.input]  # pyright: ignore[reportAssignmentType]
+        part1_iters = 10 if self.use_test_data else len(boxes)
+        circuits: list[set[tuple[int, int, int]]] = [{box} for box in boxes]
 
-        distances: deque[tuple[float, tuple[Node, Node]]] = deque()
-
-        l = len(boxes)
-
-        graph = networkx.Graph()
-        for i in range(l):
-            node1 = boxes[i]
-            graph.add_node(node1)
-            for j in range(i + 1, l):
-                node2 = boxes[j]
-                distances.append(
-                    (
-                        math.sqrt(
-                            (node1[0] - node2[0]) ** 2
-                            + (node1[1] - node2[1]) ** 2
-                            + (node1[2] - node2[2]) ** 2
-                        ),
-                        (node1, node2),
-                    )
-                )
-
-        distances = deque(sorted(distances))
+        connections = deque(
+            sorted(itertools.combinations(boxes, 2), key=lambda pair: geo3D(*pair))
+        )
 
         iters = 0
         part1 = 0
 
-        while not networkx.is_connected(graph):
+        for box1, box2 in connections:
+            circuit1 = next(c for c in circuits if box1 in c)
+            circuit2 = next(c for c in circuits if box2 in c)
             iters += 1
-            while True:
-                _, (node1, node2) = distances.popleft()
-                if node1 not in graph.neighbors(node2):
-                    graph.add_edge(node1, node2)
-                    break
-            if iters == 1000:
-                connected = (
-                    len(c)
-                    for c in sorted(
-                        networkx.connected_components(graph), key=len, reverse=True
-                    )
-                )
-                part1 = next(connected) * next(connected) * next(connected)
+            if circuit1 is circuit2:
+                continue
+            circuit1 |= circuit2
+            circuit2.clear()
+            circuits.remove(circuit2)
+            if iters == part1_iters:
+                connected = sorted((len(c) for c in circuits), reverse=True)
+                part1 = math.prod(connected[:3])
 
-        return part1, node1[0] * node2[0]
+            if len(circuits) == 1:
+                break
+
+        return part1, box1[0] * box2[0]
