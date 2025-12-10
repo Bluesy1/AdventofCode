c8d179af3ec5c5b8939df72aaa3ad5e7821c3292
Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
2025-12-09 21:45:41 -0800

    2025 Day 10 cleanups

--- solutions/2025/day_10/solution.py
+++ solutions/2025/day_10/solution.py
@@ -1,99 +1,63 @@
 # puzzle prompt: https://adventofcode.com/2025/day/10
+import z3
 
 from ...base import StrSplitSolution, answer
 from ...utils.tools import *
-from collections import deque
 
-from typing import NamedTuple
+type Machine = tuple[list[bool], list[tuple[int, ...]], list[int]]
 
-class Machine(NamedTuple):
-    target: list[bool]
-    buttons: list[tuple[int, ...]] # each button maps to a tuple of indicator indices it toggles
-    joltages: list[int]
-
-import z3
 
 class Solution(StrSplitSolution):
     _year = 2025
     _day = 10
 
-    @answer(449)
-    def part_1(self) -> int:
-        machines: list[Machine] = []
-        for block in self.input:
-            sections = block.split(" ")
-            target = [s == "#" for s in sections[0][1:-1]]
-            buttons = [tuple(nums(s)) for s in sections[1:len(sections)-1]]
-            joltages = nums(sections[-1])
-            machines.append(Machine(target, buttons, joltages))
-        
-        accumulator = 0
-        # for each machine, find the lowest number of button presses to turn on all indicators
-        for machine in machines:
-            self.debug(machine)
+    @answer((449, 17848))
+    def solve(self) -> tuple[int, int]:
+        part1 = part2 = 0
 
-            initial_state = tuple(False for _ in machine.target)
+        for machine in self.input:
+            sections = machine.split(" ")
+            target = [s == "#" for s in sections[0][1:-1]]
+            buttons = [tuple(nums(s)) for s in sections[1 : len(sections) - 1]]
+            joltages = nums(sections[-1])
+
+            # *** Part 1 ***
+
+            initial_state = tuple(False for _ in target)
             queue = deque([(initial_state, 0)])  # (state, presses)
             seen = {initial_state}
 
             while queue:
                 state, presses = queue.popleft()
-                if state == tuple(machine.target):
-                    accumulator += presses
-                    self.debug(f"Machine solved in {presses} presses")
+                if state == tuple(target):
+                    part1 += presses
                     break
 
-                for button in machine.buttons:
+                for button in buttons:
                     new_state = list(state)
                     for idx in button:
                         new_state[idx] = not new_state[idx]
                     new_state_tuple = tuple(new_state)
-                    # print(f"Pressing button toggling indicators {button}, current state {state}, new state {new_state_tuple}, presses {presses}")
                     if new_state_tuple not in seen:
                         seen.add(new_state_tuple)
                         queue.append((new_state_tuple, presses + 1))
-                        
-            
-            if len(queue) == 0:
-                self.debug("No solution found for machine")
-        
-        return accumulator
 
+            # *** Part 2 ***
 
-    @answer(17848)
-    def part_2(self) -> int:
-        machines: list[Machine] = []
-        for block in self.input:
-            sections = block.split(" ")
-            target = [s == "#" for s in sections[0][1:-1]]
-            buttons = [tuple(nums(s)) for s in sections[1:len(sections)-1]]
-            joltages = nums(sections[-1])
-            machines.append(Machine(target, buttons, joltages))
-        
-        accumulator = 0
-        # for each machine, find the lowest number of button presses to turn on all indicators
-        for machine in machines:
-            self.debug(machine)
             s = z3.Optimize()
-            num_buttons = len(machine.buttons)
-            presses = [
-                z3.Int(f"press_{i}") for i in range(num_buttons)
-            ]
+            num_buttons = len(buttons)
+            presses = [z3.Int(f"press_{i}") for i in range(num_buttons)]
             for i in range(num_buttons):
                 s.add(presses[i] >= 0)
-            for i in range(len(machine.joltages)):
+            for i in range(len(joltages)):
                 s.add(
-                    sum(presses[j] for j, button in enumerate(machine.buttons) if i in button) == machine.joltages[i]
+                    sum(presses[j] for j, button in enumerate(buttons) if i in button)
+                    == joltages[i]
                 )
             s.minimize(sum(presses))
             assert s.check() == z3.sat
             model = s.model()
-            total_presses = sum(model[b].as_long() for b in presses) # pyright: ignore[reportOptionalMemberAccess, reportAttributeAccessIssue]
-            self.debug(f"Machine solved in {total_presses} presses")
-            accumulator += total_presses
-        
-        return accumulator
+            total_presses = sum(model[b].as_long() for b in presses)  # pyright: ignore[reportOptionalMemberAccess, reportAttributeAccessIssue]
+            part2 += total_presses
 
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+        return part1, part2
